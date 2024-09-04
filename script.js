77531250187a7796f287d4fec041f9a842ff5467
PCassemiro <pedrobr2909@gmail.com>
2024-09-04 17:03:34 -0300

    Inicio
    Apenas  a base do projeto

---
+++ script.js
@@ -0,0 +1,5 @@
+window.document.querySelector('input#nome')//colocar pra n pode adicionar letra no nome
+window.document.querySelector('input#email')//definir estrutura do e-mail(. no final e o @)
+window.document.querySelector('input#cpf')//deixar no maximo 11 caracteres e colocar automatico o - e .
+window.document.querySelector('input#data')//...
+window.document.querySelector('input#senha')//colocar padrão de senha(minimo 8 e sinais)
