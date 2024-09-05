c61def0d47fc87520ea60197b1e1f1f8824d2583
PCassemiro <pedrobr2909@gmail.com>
2024-09-05 16:55:28 -0300

    Css terminado
    Aqui terminei o css e fiz uns testes no js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-window.document.querySelector('input#nome')//colocar pra n pode adicionar letra no nome
-window.document.querySelector('input#email')//definir estrutura do e-mail(. no final e o @)
-window.document.querySelector('input#cpf')//deixar no maximo 11 caracteres e colocar automatico o - e .
-window.document.querySelector('input#data')//...
-window.document.querySelector('input#senha')//colocar padrão de senha(minimo 8 e sinais)
+var inpute= window.document.querySelector('input')
+var VariavelNom= window.document.getElementById('nome')//colocar pra n pode adicionar numero no nome
+var VariavelEma= window.document.querySelector('input#email')//definir estrutura do e-mail(. no final e o @)
+var VariavelCpf= window.document.querySelector('input#cpf')//deixar no maximo 11 caracteres e colocar automatico o - e .
+var VariavelDat= window.document.querySelector('input#data')//...
+var VariavelSen= window.document.querySelector('input#senha')//colocar padrão de senha(minimo 8 e sinais)
+
+// function passar(){
+//     inpute.style.borderColor= "red";
+// }
+
+// VariavelCpf.addEventListener('input', function() {
+//     let cpf = VariavelCpf.value;
+// }
