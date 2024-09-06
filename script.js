728c5fa1d3ef659056954c523e5c47eca4b90322
PCassemiro <pedrobr2909@gmail.com>
2024-09-06 14:28:57 -0300

    projeto finalizado por enquanto
    mas sem banco pra guardar as informações

--- script.js
+++ script.js
@@ -4,11 +4,18 @@ var VariavelEma= window.document.querySelector('input#email')//definir estrutura
 var VariavelCpf= window.document.querySelector('input#cpf')//deixar no maximo 11 caracteres e colocar automatico o - e .
 var VariavelDat= window.document.querySelector('input#data')//...
 var VariavelSen= window.document.querySelector('input#senha')//colocar padrão de senha(minimo 8 e sinais)
+//função pra mudar a cor conforme passa o mouse
+function passar(){
+    inpute.style.borderColor= "red";
+}
+function mascaraCPF(input) {
+     let cpf = VariavelCpf.value;
+     if (cpf.length <= 11) {
+        cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+        cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+        cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    }
 
-// function passar(){
-//     inpute.style.borderColor= "red";
-// }
-
-// VariavelCpf.addEventListener('input', function() {
-//     let cpf = VariavelCpf.value;
-// }
+    // Atualiza o valor do campo com a máscara
+    input.value = cpf;
+}
